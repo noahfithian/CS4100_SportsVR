1e278db7a72e566dbbafdc40b65c5ffc9e8f3bf0
thomasm1248 <thomasm1248@gmail.com>
2025-04-16 11:54:15 -0600

    Copy my changes to scenes

--- Assets/Networking/Network Scripts/NetworkUIManager.cs
+++ Assets/Networking/Network Scripts/NetworkUIManager.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
 public class NetworkUIManager : MonoBehaviour
 {
-    public void JoinGame() => NetworkManager.Singleton.StartClient();
-    public void HostGame() => NetworkManager.Singleton.StartHost();
+    [SerializeField]
+    private string minigameScene;
+
+    public void JoinGame() {
+        NetworkManager.Singleton.StartClient();
+        SceneManager.LoadScene(minigameScene);
+    }
+
+    public void HostGame() {
+        NetworkManager.Singleton.StartHost();
+        SceneManager.LoadScene(minigameScene);
+    }
     
     public void QuitGameClient()
     {
